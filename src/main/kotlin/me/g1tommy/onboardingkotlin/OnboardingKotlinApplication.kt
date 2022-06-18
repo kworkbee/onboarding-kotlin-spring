a47a4728b3b1ac145d56c7c7d165d2061393c1f9
G1 <g1.tommy.jeon@gmail.com>
2022-06-18 20:50:55 +0900

    feat: Initial Commit

---
+++ src/main/kotlin/me/g1tommy/onboardingkotlin/OnboardingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package me.g1tommy.onboardingkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnboardingKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnboardingKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/me/g1tommy/onboardingkotlin/OnboardingKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.g1tommy.onboardingkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnboardingKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
